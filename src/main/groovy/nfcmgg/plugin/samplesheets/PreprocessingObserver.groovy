a3e3d67e85bbca2fa3da2febb53233b51396d068
Nicolas Vannieuwkerke <nicolas.vannieuwkerke@ugent.be>
2026-05-12 12:53:32 +0200

    change if statement to guard clause

--- src/main/groovy/nfcmgg/plugin/samplesheets/PreprocessingObserver.groovy
+++ src/main/groovy/nfcmgg/plugin/samplesheets/PreprocessingObserver.groovy
@@ -69,123 +69,122 @@ class PreprocessingObserver extends PipelineObserver {
     /* groovylint-disable-next-line MethodSize */
     @Override
     void onFlowComplete() {
-        if (session.success) {
-            entries = entries.findAll { entry ->
-                // Only retain diagnostic samples for the samplesheets
-                entry.value.get('purpose').toLowerCase() == 'diagnostic'
-            }
-            entries = entries.sort()
-
-            //
-            // nf-cmgg/sampletracking samplesheet
-            //
-            creator.dump(
-                entries
-                    .findAll { entry ->
-                        // Only create samplesheet for WES and WGS runs of DNA samples
-                        entry.value.get('type').toLowerCase() == 'dna' &&
-                        entry.value.get('tag').toLowerCase() in ['wes', 'wgs']
-                    }
-                    .values()
-                    *.subKeys([
-                        ['id', 'sample'],
-                        ['library', 'pool'],
-                        ['cram', 'sample_bam'],
-                        ['crai', 'sample_bam_index'],
-                        ['snp_cram', 'snp_bam'],
-                        ['snp_crai', 'snp_bam_index'],
-                        'sex'
-                    ]),
-                location.resolve('nfcmgg_sampletracking_samplesheet.yaml')
-            )
-
-            //
-            // nf-core/rnafusion samplesheet
-            //
-            creator.dump(
-                entries
-                    .findAll { entry ->
-                        // Only create samplesheet for RNAseqMDG runs of RNA samples
-                        entry.value.get('type').toLowerCase() == 'rna' &&
-                        entry.value.get('tag').toLowerCase() == 'rnaseqmdg'
-                    }
-                    .values()
-                    *.subKeys([
-                        ['id', 'sample'],
-                        'fastq_1',
-                        'fastq_2',
-                        'strandedness'
-                    ]),
-                location.resolve('nfcore_rnafusion_samplesheet.yaml')
-            )
-
-            //
-            // nf-cmgg/vivar samplesheet
-            //
-            creator.dump(
-                entries
-                    .findAll { entry ->
-                        // Only create samplesheet for DNA samples
-                        entry.value.get('type').toLowerCase() == 'dna'
-                    }
-                    .values()
-                    *.subKeys([
-                        'id',
-                        'organism',
-                        'tag',
-                        'binsize',
-                        ['vivar_project', 'project'],
-                        'sex',
-                        'normdup',
-                        'nipt',
-                        ['cram', 'reads'],
-                        ['crai', 'reads_index']
-                    ]),
-                location.resolve('nfcmgg_vivar_samplesheet.yaml')
-            )
-
-            //
-            // nf-cmgg/exomecnv samplesheet
-            //
-            creator.dump(
-                entries
-                    .findAll { entry ->
-                        // Only create samplesheet for WES runs of DNA samples
-                        entry.value.get('type').toLowerCase() == 'dna' &&
-                        entry.value.get('tag').toLowerCase() in ['wes']
-                    }
-                    .values()
-                    *.subKeys([
-                        ['id', 'sample'],
-                        ['exomecnv_batch', 'batch'],
-                        'family',
-                        'cram',
-                        'crai',
-                        ['per_base_bed', 'bed'],
-                        ['per_base_bed_index', 'bed_index']
-                    ]),
-                location.resolve('nfcmgg_exomecnv_samplesheet.yaml')
-            )
-
-            //
-            // nf-cmgg/smallvariants samplesheet
-            //
-            creator.dump(
-                entries
-                    .findAll { entry ->
-                        // Only create samplesheet for DNA samples
-                        entry.value.get('type').toLowerCase() == 'dna' &&
-                        entry.value.get('tag').toLowerCase() in ['wes', 'wgs']
-                    }
-                    .values()
-                    *.subKeys([
-                        ['id', 'sample'],
-                        'cram',
-                        'crai'
-                    ]),
-                location.resolve('nfcmgg_smallvariants_samplesheet.yaml')
-            )
+        if (!session.success) { return }
+        entries = entries.findAll { entry ->
+            // Only retain diagnostic samples for the samplesheets
+            entry.value.get('purpose').toLowerCase() == 'diagnostic'
         }
+        entries = entries.sort()
+
+        //
+        // nf-cmgg/sampletracking samplesheet
+        //
+        creator.dump(
+            entries
+                .findAll { entry ->
+                    // Only create samplesheet for WES and WGS runs of DNA samples
+                    entry.value.get('type').toLowerCase() == 'dna' &&
+                    entry.value.get('tag').toLowerCase() in ['wes', 'wgs']
+                }
+                .values()
+                *.subKeys([
+                    ['id', 'sample'],
+                    ['library', 'pool'],
+                    ['cram', 'sample_bam'],
+                    ['crai', 'sample_bam_index'],
+                    ['snp_cram', 'snp_bam'],
+                    ['snp_crai', 'snp_bam_index'],
+                    'sex'
+                ]),
+            location.resolve('nfcmgg_sampletracking_samplesheet.yaml')
+        )
+
+        //
+        // nf-core/rnafusion samplesheet
+        //
+        creator.dump(
+            entries
+                .findAll { entry ->
+                    // Only create samplesheet for RNAseqMDG runs of RNA samples
+                    entry.value.get('type').toLowerCase() == 'rna' &&
+                    entry.value.get('tag').toLowerCase() == 'rnaseqmdg'
+                }
+                .values()
+                *.subKeys([
+                    ['id', 'sample'],
+                    'fastq_1',
+                    'fastq_2',
+                    'strandedness'
+                ]),
+            location.resolve('nfcore_rnafusion_samplesheet.yaml')
+        )
+
+        //
+        // nf-cmgg/vivar samplesheet
+        //
+        creator.dump(
+            entries
+                .findAll { entry ->
+                    // Only create samplesheet for DNA samples
+                    entry.value.get('type').toLowerCase() == 'dna'
+                }
+                .values()
+                *.subKeys([
+                    'id',
+                    'organism',
+                    'tag',
+                    'binsize',
+                    ['vivar_project', 'project'],
+                    'sex',
+                    'normdup',
+                    'nipt',
+                    ['cram', 'reads'],
+                    ['crai', 'reads_index']
+                ]),
+            location.resolve('nfcmgg_vivar_samplesheet.yaml')
+        )
+
+        //
+        // nf-cmgg/exomecnv samplesheet
+        //
+        creator.dump(
+            entries
+                .findAll { entry ->
+                    // Only create samplesheet for WES runs of DNA samples
+                    entry.value.get('type').toLowerCase() == 'dna' &&
+                    entry.value.get('tag').toLowerCase() in ['wes']
+                }
+                .values()
+                *.subKeys([
+                    ['id', 'sample'],
+                    ['exomecnv_batch', 'batch'],
+                    'family',
+                    'cram',
+                    'crai',
+                    ['per_base_bed', 'bed'],
+                    ['per_base_bed_index', 'bed_index']
+                ]),
+            location.resolve('nfcmgg_exomecnv_samplesheet.yaml')
+        )
+
+        //
+        // nf-cmgg/smallvariants samplesheet
+        //
+        creator.dump(
+            entries
+                .findAll { entry ->
+                    // Only create samplesheet for DNA samples
+                    entry.value.get('type').toLowerCase() == 'dna' &&
+                    entry.value.get('tag').toLowerCase() in ['wes', 'wgs']
+                }
+                .values()
+                *.subKeys([
+                    ['id', 'sample'],
+                    'cram',
+                    'crai'
+                ]),
+            location.resolve('nfcmgg_smallvariants_samplesheet.yaml')
+        )
     }
 
     @Override
