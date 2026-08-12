0ec8e1ca7094992fe17659c2b9f8898d80d994ab
Nicolas Vannieuwkerke <nicolas.vannieuwkerke@ugent.be>
2026-08-12 17:18:59 +0200

    add top-level worksheet validation

---
+++ src/main/groovy/nfcmgg/plugin/worksheet/Worksheet.groovy
@@ -0,0 +1,50 @@
+package nfcmgg.plugin.worksheet
+
+import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j
+
+import org.yaml.snakeyaml.Yaml
+import java.nio.file.Path
+
+
+/**
+ * A class used to define the structure of the worksheet
+ */
+@CompileStatic
+@Slf4j
+class Worksheet {
+
+    final String name
+    final String idField
+    final WorksheetInput input
+    final WorksheetValues values
+    final WorksheetOutput output
+    final WorksheetMetrics metrics
+    final WorksheetSamplesheets samplesheets
+
+    Worksheet(Path worksheet) {
+        final Map worksheetMap = new Yaml().load(worksheet.text)
+        if (worksheetMap.name == null) {
+            error("Worksheet ${worksheet.name} is missing the required 'name' field")
+        }
+        name = worksheetMap.name
+        if (worksheetMap.id_field == null) {
+            error("Worksheet ${worksheet.name} is missing the required 'id_field' field")
+        }
+        idField = worksheetMap.id_field
+        if (worksheetMap.input == null) {
+            error("Worksheet ${worksheet.name} is missing the required 'input' field")
+        }
+        input = new WorksheetInput(worksheetMap.input as Map)
+        values = worksheetMap.values != null ? new WorksheetValues(worksheetMap.values as Map) : null
+        if (worksheetMap.output == null) {
+            error("Worksheet ${worksheet.name} is missing the required 'output' field")
+        }
+        output = new WorksheetOutput(worksheetMap.output as Map)
+        metrics = worksheetMap.metrics != null ?new WorksheetMetrics(worksheetMap.metrics as Map) : null
+        if (worksheetMap.samplesheets == null) {
+            error("Worksheet ${worksheet.name} is missing the required 'samplesheets' field")
+        }
+        samplesheets = new WorksheetSamplesheets(worksheetMap.samplesheets as List<Map>)
+    }
+}
